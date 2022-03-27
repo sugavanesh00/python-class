ebaa2288dccec7b455b5ef0d4beb89d296196c00
sugavanesh00 <101034052+sugavanesh00@users.noreply.github.com>
2022-03-27 12:47:38 +0530

    Add files via upload

---
+++ add sub mul div.php
@@ -0,0 +1,14 @@
+a=int(input("enter the value:"))
+b=int(input("enter the value:"))
+sum=a+b
+difference=a-b
+multiply=a*b
+division=a/b
+print("sum of the value:",(sum))
+print("difference of value:",(difference))
+print("multiply of value:",(multiply))
+print("division of value:",(division))
+print("sum of the values {} and {} is  {}".format(a,b,sum))
+print("difference between values  {} and {} is  {}".format(a,b,difference))
+print("multiply of the values {} and {} is  {}".format(a,b,multiply))
+print("division of the values {} and {} is  {}".format(a,b,division))
